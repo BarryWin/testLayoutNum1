99fc71f7b491b1966fb5a13fd146fc1732b93dce
BarryWin <43175007+BarryWin@users.noreply.github.com>
2019-02-20 23:23:26 +0300

    carousel done!

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
+    var slideIndex = 0;
+    showSlides();
+
+    function showSlides() {
+        for (var i = 0; i < $('.slides').length; i++){
+            $('.slides')[i].style.display = 'none';
+        }
+        slideIndex++
+        if (slideIndex > 3){
+            slideIndex = 1;
+        }
+        $('.slides')[slideIndex - 1].style.display = 'block';
+        setTimeout(showSlides, 6000);
+    }
 
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     browserSync = require('browser-sync'),
-    del = require('del');
+    del = require('del'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglifyjs'),
+    autoprefixer  = require('gulp-autoprefixer');
 
 gulp.task('sass', function () {
     return gulp.src('app/sass/*.+(sass|scss)')
         .pipe(sass({outputStyle:'expanded'})).on('error',sass.logError)
+        .pipe(autoprefixer(['last 5 versions']))
         .pipe(gulp.dest('app/css'))
         .pipe(browserSync.reload({stream: true}))
 });
@@ -20,14 +24,27 @@ gulp.task('browser-sync', function () {
     })
 });
 
+gulp.task('scripts', function() {
+    return gulp.src([
+        'app/libs/jquery/dist/jquery.min.js',
+        'app/libs/bootstrap/dist/js/bootstrap.min.js', // Always at the end
+    ])
+        .pipe(concat('libs.min.js'))
+        .pipe(uglify()) // Minify js (opt.)
+        .pipe(gulp.dest('app/js'))
+        .pipe(browserSync.reload({ stream: true }))
+});
+
 gulp.task('clean', async function() {
     return del.sync('dist');
 });
+
 gulp.task('watch', function () {
     gulp.watch('app/sass/*.+(sass|scss)', gulp.parallel('sass'));
     gulp.watch('app/**/*.+(html|js)').on('change', browserSync.reload);
 });
-gulp.task('run', gulp.parallel('browser-sync', 'sass', 'watch'));
+
+gulp.task('run', gulp.parallel('browser-sync', 'sass','scripts','watch'));
 gulp.task('build',async function() {
    var buildCss = gulp.src('app/css/*.css')
        .pipe(gulp.dest('dist/css'));
@@ -38,5 +55,5 @@ gulp.task('build',async function() {
    var buildFonts = gulp.src('app/fonts/**/*')
        .pipe(gulp.dest('dist/fonts'));
 });
-gulp.task('builder', gulp.series('clean','sass','build'));
+gulp.task('builder', gulp.series('clean','sass','scripts','build'));
 gulp.task('default',gulp.series('run'));
